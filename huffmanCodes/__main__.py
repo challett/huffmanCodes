2c9b30a05e46591d1fadeabdc61e19ae6972259d
Connor Hallett <connor.hallett@gmail.com>
2017-03-14 11:54:16 -0400

    Change list order

--- huffmanCodes/__main__.py
+++ huffmanCodes/__main__.py
@@ -5,14 +5,18 @@ import optparse
 
 log = logging.getLogger(__name__)
 
+# Insert a pObj such that the sorting of the list is persisted
 def insert_p(list, pObj):
     for index, value in enumerate(list):
-        if value['p'] < pObj['p']:
+        if value['p'] > pObj['p']:
             list.insert(index, pObj)
-            break
+            return
+    list.append(pObj)
+    return
+
 
 def main(probabilities):
-    isTakingInputs = True
+    # Array is to be sorted by increasing probability.  Lowest prob will be at index 0.
     probArray = []
 
     while True:
@@ -24,13 +28,12 @@ def main(probabilities):
         if len(splitString) !== 2:
             print "Invalid Input\n"
             continue
-
-        probArray.append(dict(
+        pObj = dict(
             symbol=splitString[0],
             p=float(splitString[1]),
-        ))
+        )
+        insert_p(probArray, pObj)
 
-    sortedArray = sorted(probArray, key=lambda k: -k['p'])
     #TODO: Run Huffman Algorithm to find codes
     return 0
 
