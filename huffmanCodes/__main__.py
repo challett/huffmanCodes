053ddedd96387a62f7d1418af65472ee11a3e974
Connor Hallett <connor.hallett@gmail.com>
2017-03-14 14:11:31 -0400

    HardCoded inputs for testing

--- huffmanCodes/__main__.py
+++ huffmanCodes/__main__.py
@@ -13,13 +13,22 @@ class HuffTree:
         self.right = right
         self.code = ''
 
-    def printCodes(self, code=''):
+    def setCodes(self, code=''):
+        self.code = code
         if (self.left):
-            self.left.printCodes(code + '1')
+            print "Calling left with code " + code+"1"
+            self.left.setCodes(code+"1")
         if (self.right):
-            self.right.printCodes(code + '0')
+            print "Calling right with code " + code+"0" + " From " + self.symbol
+            self.right.setCodes(code+"0")
+
+    def printCodes(self):
+        if (self.left):
+            self.left.printCodes()
+        if (self.right):
+            self.right.printCodes()
         else:
-            print self.symbol + ' - ' + code
+            print self.symbol + " - " + self.code
 
 
 # Insert a pObj such that the sorting of the list is persisted
@@ -36,28 +45,60 @@ def main():
     # Array is to be sorted by increasing probability.  Lowest prob will be at index 0.
     huffList = []
 
-    while True:
-        inputString = raw_input("Enter probabilities as symbol:p, enter done when done\n")
-        splitString = inputString.split(':')
-        if inputString == "done":
-            break
+    # while True:
+    #     inputString = raw_input("Enter probabilities as symbol=p, enter done when done\n")
+    #     splitString = inputString.split('=')
+    #     if inputString == "done":
+    #         break
+    #
+    #     if len(splitString) != 2:
+    #         print "Invalid Input\n"
+    #         continue
+    #
+    #     newTree = HuffTree(symbol=splitString[0], p=float(splitString[1]))
 
-        if len(splitString) != 2:
-            print "Invalid Input\n"
-            continue
 
-        newTree = HuffTree(symbol=splitString[0], p=splitString[1])
-        insert_p(huffList, newTree)
+
+    newTree = HuffTree(symbol="AA", p=9./16)
+    insert_p(huffList, newTree)
+
+    newTree = HuffTree(symbol="AB", p=9./64)
+    insert_p(huffList, newTree)
+
+    newTree = HuffTree(symbol="AC", p=3./64)
+    insert_p(huffList, newTree)
+
+    newTree = HuffTree(symbol="BA", p=9./64)
+    insert_p(huffList, newTree)
+
+    newTree = HuffTree(symbol="BB", p=9./256)
+    insert_p(huffList, newTree)
+
+    newTree = HuffTree(symbol="BC", p=3./256)
+    insert_p(huffList, newTree)
+
+    newTree = HuffTree(symbol="CA", p=3./64)
+    insert_p(huffList, newTree)
+
+    newTree = HuffTree(symbol="CB", p=3./256)
+    insert_p(huffList, newTree)
+
+    newTree = HuffTree(symbol="CC", p=1./256)
+    insert_p(huffList, newTree)
 
     while len(huffList) > 1:
         lo = huffList.pop()
         hi = huffList.pop()
         newP = lo.p + hi.p
-        combined = HuffTree(p=newP, left=hi, right=lo)
+        #print "merging " + hi.symbol + " with " + lo.symbol
+        combined = HuffTree(p=newP, left=hi, right=lo, symbol=hi.symbol + lo.symbol)
+        #print "combined contains left=" + combined.left.symbol + " right=" + combined.right.symbol
         insert_p(huffList, combined)
 
     if len(huffList):
+        huffList[0].setCodes()
         huffList[0].printCodes()
+
 
     return 0
 
